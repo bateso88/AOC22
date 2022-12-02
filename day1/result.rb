9bf2ec13a80a527ed2ba452017dbeff4f695af9f
y <y>
2022-12-02 10:21:34 +0000

    [main] Complete d1c2

--- day1/result.rb
+++ day1/result.rb
@@ -1,3 +1,5 @@
 calories_per_elf = File.read("input.txt").split("\n\n").map{|elf| elf.split.map(&:to_i).sum }
 
-puts calories_per_elf.max
+p calories_per_elf.max
+
+p calories_per_elf.sort.last(3).sum
