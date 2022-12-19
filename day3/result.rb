710206b06ee96a28f8f731e9298294ee372ec5d5
y <y>
2022-12-19 10:29:31 +0000

    [main] Complete d3c2

--- day3/result.rb
+++ day3/result.rb
@@ -1,6 +1,6 @@
 rucksacks = File.read("input.txt").split.map { |rucksack| rucksack.chars.each_slice(rucksack.length / 2).to_a }
 
-incorrect_items = rucksacks.map { |rucksack| rucksack[0].select { |item| rucksack[1].include?(item)}[0] }
+incorrect_items = rucksacks.map { |rucksack| rucksack[0].select { |item| rucksack[1].include?(item) }[0] }
 
 capital_letters = incorrect_items.select {|item| item.upcase == item}
 lower_case_letters = incorrect_items.select {|item| item.upcase != item}
@@ -9,6 +9,13 @@ result = capital_letters.map { |char| char.ord - 38 }.sum + lower_case_letters.m
 
 p result
 
-groups = File.read("input.txt").split.each_slice(3).to_a
+groups = File.read("input.txt").split.each_slice(3).map { |group| group.map(&:chars) }
 
-p groups
+badges = groups.map { |group| group[0].select { |item| group[1].include?(item) && group[2].include?(item) }[0] }
+
+capital_letters = badges.select {|item| item.upcase == item}
+lower_case_letters = badges.select {|item| item.upcase != item}
+
+result = capital_letters.map { |char| char.ord - 38 }.sum + lower_case_letters.map { |char| char.ord - 96 }.sum
+
+p result
